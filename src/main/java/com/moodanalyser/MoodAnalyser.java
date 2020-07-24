7b960a9c922f837a99c309e6515f6ccdd31d08bc
sonvin2916 <sonalchougule07@gmail.com>
2020-07-24 17:11:50 +0530

    [Sonal]refactored : Inform user if entered invalid mood

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -2,58 +2,29 @@ package com.moodanalyser;
 
 import com.moodanalyser.com.moodanalyser.MoodAnalysisException;
 
-public class MoodAnalyser
-{
-    String message;
+public class MoodAnalyser {
+    private String message;
+
     public MoodAnalyser()
     {
 
     }
-    public MoodAnalyser(String message)
-    {
+
+    public MoodAnalyser(String message) {
         this.message = message;
     }
-    public String analyseMood(String message)
-    {
-        return "SAD";
-    }
-    public String analyseMoodAgain(String message)
-    {
-            if (message.contains("SAD"))
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length()==0) {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Enter a proper message");
+            }
+            if(message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
-    }
-
-    public String analyseMood() throws MoodAnalysisException
-    {
-        try
-        {
-            if (message.contains("SAD"))
-                return "SAD";
-            else
-                return "HAPPY";
-        }
-        catch (NullPointerException e)
-        {
-            return "HAPPY";
-
+        }catch (NullPointerException e){
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Enter a proper message");
         }
     }
-
-    public String analyseMoodAgain() throws MoodAnalysisException {
-        try
-        {
-            if (message.contains("SAD"))
-                return "SAD";
-            else
-                return "HAPPY";
-        }
-        catch (NullPointerException e)
-        {
-            throw new MoodAnalysisException("Please enter proper message");
-        }
-    }
-
-
 }
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,51 +1,49 @@
 package com.moodanalyser;
 
+import com.moodanalyser.MoodAnalyser;
 import com.moodanalyser.com.moodanalyser.MoodAnalysisException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class MoodAnalyserTest
-{
+public class MoodAnalyserTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalysisException
-    {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+    public void  givenAnalyseMood_WhenSad_ShouldReturnSad() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser= new MoodAnalyser("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",mood);
     }
+
     @Test
-    public void givenMessage_WhenNotSad_ShouldReturnHappy() throws MoodAnalysisException
-    {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMoodAgain("I am in Happy Mood");
-        Assert.assertEquals("HAPPY",mood);
-    }
-    @Test
-    public void givenNullMood_ShouldReturnHappy() throws MoodAnalysisException
-    {
-        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+    public void givenAnalyseMood_WhenHappy_ShouldReturnHappy() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser= new MoodAnalyser("I am in Happy Mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",mood);
+
     }
     @Test
-    public void givenNullMood_ShouldThrowException()
-    {
+    public void givenAnalyseMood_WhenNull_ShouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = null;
         try
         {
-            ExpectedException exceptionRule = ExpectedException.none();
-            exceptionRule.expect(MoodAnalysisException.class);
-            mood = moodAnalyser.analyseMoodAgain();
-            Assert.assertEquals("HAPPY",mood);
+            moodAnalyser.analyseMood();
         }
-        catch(MoodAnalysisException e)
+        catch (MoodAnalysisException e)
         {
-            e.printStackTrace();
-            
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL,e.getType());
         }
-
     }
-}
+    @Test
+    public void givenAnalyseMood_WhenEpty_ShouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try
+        {
+            moodAnalyser.analyseMood();
+        }
+        catch (MoodAnalysisException e)
+        {
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,e.getType());
+        }
+    }
 
+}
