6c3ca32c7d4232c1b6eabe694408f72fbfc5dbb4
sonvin2916 <sonalchougule07@gmail.com>
2020-07-24 16:47:02 +0530

    [Sonal]refactored : In case of Null throw exception

--- src/main/java/com/moodanalyser/com/moodanalyser/MoodAnalysisException.java
+++ src/main/java/com/moodanalyser/com/moodanalyser/MoodAnalysisException.java
@@ -3,20 +3,11 @@ import com.moodanalyser.com.moodanalyser.MoodAnalysisException;
 
 public class MoodAnalysisException extends Exception
 {
-//    public enum ExceptionType
-//    {
-//        ENTERED_NULL,ENTERED_EMPTY;
-//    }
-//    private ExceptionType type;
-//    public ExceptionType getType()
-//    {
-//        return type;
-//    }
+
     public MoodAnalysisException( String message)
     {
         super(message);
-        //this.type=type;
-
+        
     }
 
 }
